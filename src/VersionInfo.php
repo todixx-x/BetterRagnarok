5ec790adefe401192d5b09cabedfb261a1a065f6
todixx-x <blissmcpe@proton.me>
2026-08-06 21:59:08 +0100

    Update VersionInfo.php

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -34,7 +34,7 @@ use function str_repeat;
 final class VersionInfo{
 	public const NAME = "Ragnarok";
 	public const BASE_VERSION = "5.44.2";
-	public const IS_DEVELOPMENT_BUILD = false;
+	public const IS_DEVELOPMENT_BUILD = true;
 	public const BUILD_CHANNEL = "master";
 	public const GITHUB_URL = "https://github.com/todixx-x/BetterRagnarok";
 
