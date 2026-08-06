84cc1a3e781c4b5b1a1c3920663197808b913742
todixx-x <blissmcpe@proton.me>
2026-08-07 00:03:49 +0100

    Set IS_DEVELOPMENT_BUILD to false

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -34,7 +34,7 @@ use function str_repeat;
 final class VersionInfo{
 	public const NAME = "BetterRagnarok";
 	public const BASE_VERSION = "5.44.1";
-	public const IS_DEVELOPMENT_BUILD = true;
+	public const IS_DEVELOPMENT_BUILD = false;
 	public const BUILD_CHANNEL = "master";
 	public const GITHUB_URL = "https://github.com/todixx-x/BetterRagnarok";
 
